48b193113d83fa42fd8cd2b48a7bcdc6e7c92ffa
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-07-24 09:56:46 -0600

    creacion inicial

---
+++ amma20240724/src/main/java/com/amma20240724/amma20240724/Amma20240724Application.java
@@ -0,0 +1,13 @@
+package com.amma20240724.amma20240724;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amma20240724Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amma20240724Application.class, args);
+	}
+
+}
---
+++ amma20240724/src/test/java/com/amma20240724/amma20240724/Amma20240724ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amma20240724.amma20240724;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amma20240724ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
